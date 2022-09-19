a6e665340c063be74f828ee9cb7c47ae64a84bd1
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-09-19 19:34:06 +0100

    feat: updater server :octocat:

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const port = process.env.PORT;
+const port = 3000;
 app.listen(port, () => console.log(`App running on port ${port}...`));
